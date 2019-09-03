b037610f1e35e2c2835acb99910e6c355c49d49d
isuriimalsha <isuriimalsha94@gmail.com>
2019-09-03 11:34:29 +0530

    First commit

---
+++ src/taktiktuk/TakTikTuk.java
@@ -0,0 +1,18 @@
+
+package taktiktuk;
+
+/**
+ *
+ * @author Isuri Imalsha
+ */
+public class TakTikTuk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame mf = new MainFrame();
+        mf.setVisible(true);
+    }
+    
+}
